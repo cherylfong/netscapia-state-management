f55d32fec34f9729ea2a4bcc3aa22edb85f0ed38
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-03 16:26:55 +0000

    add code and comments from section 'Testing Zustand stores' from part6 sub b

---
+++ counter/src/store.test.js
@@ -0,0 +1,46 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { renderHook, act } from '@testing-library/react'
+import useCounterStore, { useCounter, useCounterControls } from './store'
+
+beforeEach(() => {
+  useCounterStore.setState({ counter: 0 })
+})
+
+describe('counter hooks', () => {
+  it('useCounter returns initial value of 0', () => {
+    const { result } = renderHook(() => useCounter())
+    expect(result.current).toBe(0)
+  })
+
+  it('increment updates counter', () => {
+    const { result: counter } = renderHook(() => useCounter())
+    const { result: controls } = renderHook(() => useCounterControls())
+
+    // act is a helper function that ensures all state updates and their side effects have been processed before the test code continues
+    act(() => controls.current.increment())
+
+    expect(counter.current).toBe(1)
+  })
+
+  it('decrement updates counter', () => {
+    const { result: counter } = renderHook(() => useCounter())
+    const { result: controls } = renderHook(() => useCounterControls())
+
+    act(() => controls.current.decrement())
+
+    expect(counter.current).toBe(-1)
+  })
+
+  it('zero resets counter', () => {
+    const { result: counter } = renderHook(() => useCounter())
+    const { result: controls } = renderHook(() => useCounterControls())
+
+    act(() => {
+      controls.current.increment()
+      controls.current.increment()
+      controls.current.zero()
+    })
+
+    expect(counter.current).toBe(0)
+  })
+})
