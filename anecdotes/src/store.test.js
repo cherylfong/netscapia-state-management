1bb71fdb08e14ce9926baf3de0b0eaa572b1b5b0
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-03 18:17:04 +0000

    complete exercise 6.13

--- anecdotes/src/store.test.js
+++ anecdotes/src/store.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest'
-import { renderHook, act } from '@testing-library/react'
+import { render, renderHook, screen, act } from '@testing-library/react'
+import { createElement } from 'react'
 
 vi.mock('./services/anecdote', () => ({
     default: {
@@ -11,6 +12,7 @@ vi.mock('./services/anecdote', () => ({
 
 import anecdoteService from './services/anecdote'
 import useAnecdoteStore, { useAnecdotes, useFilter, useAnecdotesActions } from './store'
+import AnecdoteList from './components/AnecdoteList'
 
 
 beforeEach(() => {
@@ -32,7 +34,26 @@ describe('useNoteActions', () => {
             await result.current.initialize()
         })
 
-       const { result: anecdotesResult } = renderHook(() => useAnecdotes())
+        const { result: anecdotesResult } = renderHook(() => useAnecdotes())
         expect(anecdotesResult.current).toEqual(mockAnecdotes)
+    })
+
+    it('the component that displays anecdotes from store is sorted by votes', () => {
+
+        const mockAnecdotes = [{ id: 1, content: 'Test1', votes: 1 }, { id: 2, content: 'Test2', votes: 2 }, { id: 3, content: 'Test3', votes: 3 }]
+
+        useAnecdoteStore.setState({ anecdotes: mockAnecdotes, filter: '' })
+        render(createElement(AnecdoteList))
+
+        const displayedContents = screen
+            .getAllByText(/^Test[123]$/)
+            .map(element => element.textContent)
+
+        expect(displayedContents).toEqual(['Test3', 'Test2', 'Test1'])
+
+        // to extra verify that the values diplayed 
+        // are from the Zustand store through setState() and not via API calls
+        expect(anecdoteService.getAll).not.toHaveBeenCalled()
+
     })
 }) // useNoteActions describe
