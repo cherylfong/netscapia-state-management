88d28e076af41a77e9a74fc4e7c276eb8f0c6228
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-07 15:07:07 +0000

    refactor TanStack handling into a separate file or module to be used in App from part6 sub c titled 'useNotes custom hook'

--- notes-query/src/App.jsx
+++ notes-query/src/App.jsx
@@ -1,50 +1,20 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getNotes, createNote, updateNote } from './requests'
+import { useNotes } from './hooks/useNotes'
 
 const App = () => {
-  const queryClient = useQueryClient()
+  const { notes, isPending, addNote: addNoteToServer, toggleImportance } = useNotes()
 
-  const newNoteMutation = useMutation({
-    mutationFn: createNote,
-
-    // manual update of notes rendering
-    onSuccess: (newNote) => {
-      const notes = queryClient.getQueryData(['notes'])
-      queryClient.setQueryData(['notes'], notes.concat(newNote))
-    },
-  })
-
-  const updateNoteMutation = useMutation({
-    mutationFn: updateNote,
-    // causes TanStack Query to automatically update a query with the key notes
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] })
-    }
-  })
-
-  const result = useQuery({
-    queryKey: ['notes'],
-    queryFn: getNotes,
-    // turn off fetching when window refocuses (default behavior)
-    refetchOnWindowFocus: false
-  })
-
+  // assign addNote from useNotes as the function name addNoteToServer
+  // const addNote is a local variable in App
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.reset()
-    newNoteMutation.mutate({ content, important: true })
+    addNoteToServer(content)
   }
 
-  const toggleImportance = (note) => {
-    updateNoteMutation.mutate({ ...note, important: !note.important })
-  }
-
-  if (result.isPending) {
+  if (isPending) {
     return <div>loading data...</div>
   }
-
-  const notes = result.data
 
   return (
     <div>
@@ -54,9 +24,9 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       {notes.map((note) => (
-        <li key={note.id} onClick={() => toggleImportance(note)}>
+        <li key={note.id}>
           {note.important ? <strong>{note.content}</strong> : note.content}
-          <button onClick={() => toggleImportance(note.id)}>
+          <button onClick={() => toggleImportance(note)}>
             {note.important ? 'make not important' : 'make important'}
           </button>
         </li>
